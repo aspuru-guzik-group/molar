e0c5c08c291df6b10399552b2dfe5f709ea689ef
Ian Benlolo <ian.benlolo@mail.mcgill.ca>
2021-05-26 11:49:16 -0400

    dockerfile creates, need to work on entrypoint

--- tests/backend/endpoints/test_user.py
+++ tests/backend/endpoints/test_user.py
@@ -1,2 +1,11 @@
-def test_get_users(client):
+def add_user(client, user, password, )
+    
+
+def test_get_users(client, username = None, password = None, database = 'molar_main'):
+    if username if None:
+        username = os.getenv("MOLAR_SUPERUSER") or "test@molar.tooth"
+    if password is None:
+        password = os.getenv('MOLAR_PASSWORD') or 'tooth'
+
+    out = client.get('/api/v1/usets/get-users/', )
     pass
