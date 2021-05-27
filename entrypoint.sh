3713448711e8a3feb61829abcbe33bb86a7ff86e
Théophile Gaudin <theophile.gaudin@gmail.com>
2021-05-27 17:41:23 +0200

    :whale: backend entrypoint wait for postgres to accept connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 export PGPASSWORD=$POSTGRES_PASSWORD
+
+MAX_RETRIES=10
+while ! pg_isready -q -h $POSTGRES_SERVER; do
+  sleep 5
+  MAX_RETRIES=$((MAX_RETRIES - 1))
+  if [[ MAX_RETRIES -eq 0 ]]; then
+    echo "Could not connect to database"
+    exit
+  fi
+
+done
+
 DATABASES=$(psql -h localhost -U postgres -l | grep UTF8 | awk '{print $1}')
 
 if ! echo $DATABASES | grep -w -q molar_main > /dev/null; then 
