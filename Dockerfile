ac0c0599f9de847ec7e6c949be5d4b58f9d3db25
Théophile Gaudin <theophile.gaudin@gmail.com>
2021-05-27 16:38:16 +0200

    :construction: :whale: getting the docker-compose.yml ready

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,12 @@ FROM python:3.7-slim
 
 LABEL maintainer="Ian Benlolo <ian.benlolo@gmail.com> Theophile Gaudin <tgaudin@cs.toronto.edu>"
 
+RUN apt-get update && \
+    apt-get install -y postgresql-client && \
+    rm -rf /var/lib/apt/lists/*
+
 RUN pip install --upgrade pip && \
     pip install molar[backend]
 
 COPY entrypoint.sh .
-ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["/bin/bash", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
+#!/bin/sh
+
+export PGPASSWORD=$POSTGRES_PASSWORD
 DATABASES=$(psql -h localhost -U postgres -l | grep UTF8 | awk '{print $1}')
 
-
-if [[ ! $(echo $DATABASES | grep -w -q molar_main) ]]; then 
+if ! echo $DATABASES | grep -w -q molar_main > /dev/null; then 
   molarcli install remote \
     --hostname $POSTGRES_SERVER \
     --postgres-username $POSTGRES_USER \
     --postgres-password $POSTGRES_PASSWORD 
 fi
-
 
 uvicorn molar.backend.main:app
