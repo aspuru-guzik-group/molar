1fcdfc900dca7745f124c42ad68697c63e6106fc
Théophile Gaudin <theophile.gaudin@gmail.com>
2021-06-02 17:01:49 +0200

    removing SERVER_NAME

--- molar/backend/core/config.py
+++ molar/backend/core/config.py
@@ -12,7 +12,6 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 360
-    SERVER_NAME: str
     SERVER_HOST: AnyHttpUrl
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
